a75997708451880fb10ea0b96c662e3bf6094cda
Michael Henry Davies <michael_methodtheatre.com>
2025-04-22 21:43:42 -0600

    db fixes

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,16 +1,22 @@
-import models from '../models/index.js';
-import db from '../config/connection.js';
+import { Question } from '../models/index.js';
+import questions from './pythonQuestions.json' with { type: 'json' };
 
-export default async (modelName: "Question", collectionName: string) => {
+export default async () => {
+  // try {
+  //   let modelExists = await models[modelName].db.db.listCollections({
+  //     name: collectionName
+  //   }).toArray()
+
+  //   if (modelExists.length) {
+  //     await db.dropCollection(collectionName);
+  //   }
+  // } catch (err) {
+  //   throw err;
+  // }
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
-
-    if (modelExists.length) {
-      await db.dropCollection(collectionName);
-    }
+    await Question.deleteMany({});
+    await Question.insertMany(questions);
   } catch (err) {
-    throw err;
+    console.log(err);
   }
 }
